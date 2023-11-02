0215b05598d1dd8e00aedb9d238d70b773e0a1c0
zain <zainsattar21@gmail.com>
2023-11-02 19:16:12 +0500

    Sign Up static page complete

--- src/Components/SignupForm.jsx
+++ src/Components/SignupForm.jsx
@@ -1,34 +1,139 @@
-const SignupForm = () => {
-  return (
-    <div>
-      <form>
-        <div className="input">
-          <label htmlFor="name">
-            Name
-            <input type="text" />
-          </label>
-        </div>
-        <div className="input">
-          <label htmlFor="email address">
-            Email
-            <input type="email" />
-          </label>
-        </div>
-        <div className="input">
-          <label htmlFor="password">
-            Password
-            <input type="password" />
-          </label>
-        </div>
-        <div className="input">
-          <label htmlFor="password">
-            Confirm Password
-            <input type="password" />
-          </label>
-        </div>
-      </form>
-    </div>
-  );
-};
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-export default SignupForm;
+function Copyright(props) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+// TODO remove, this demo shouldn't need to reset the theme.
+
+const defaultTheme = createTheme();
+
+export default function SignIn() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+  };
+
+  return (
+    <ThemeProvider
+      theme={defaultTheme}
+      style={
+        "https://www.sandyapps.co/wp-content/uploads/2023/02/logo-dark-scaled.webp"
+      }
+    >
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "white" }}>
+            <img src="/src/assets/logo.png" width="28px" height="auto" />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign Up
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="name"
+              label="User Name"
+              name="name"
+              autoComplete="text"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="confirm-password"
+              label="confirm-Password"
+              type="confirm-password"
+              id="confirm-password"
+              autoComplete="current-password"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item></Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Already have an account? login"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
+  );
+}
