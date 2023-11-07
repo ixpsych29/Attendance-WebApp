9406b89f97a84a407d673d8e6b3e2e9769eb8f8a
zain <zainsattar21@gmail.com>
2023-11-07 17:55:17 +0500

    navbar complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 // import "./App.css";
 
 import DisplayCard from "./Components/DisplayCard";
+import { Navbar } from "./Components/Navbar";
 // import LoginForm from "./Components/LoginForm";
 // import SignupForm from "./Components/SignupForm";
 function App() {
   return (
     <>
+      <Navbar />
       {/* <LoginForm />
       <SignupForm /> */}
       <DisplayCard />
