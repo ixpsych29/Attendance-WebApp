668d1bfc0b17cff325425e55647226f1289576ab
zain <zainsattar21@gmail.com>
2023-12-05 16:28:05 +0500

    server.js updated

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,11 +1,14 @@
-const express = require('express')
-const app = express()
-const port = process.env.port || 3000;
+require("dotenv").config();
+const express = require("express");
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+  // res.send("Hello World!");
+  res.json({ msg: "hellooo i am here to listen you request" });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
