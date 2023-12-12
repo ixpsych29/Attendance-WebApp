51e1accc887e317b340b77f65b2fd0e9b9f5db63
Psych <abdullahbinrafique945@gmail.com>
2023-12-12 13:04:26 +0500

    added default date on Attendance module

--- FrontEnd/src/Components/DatePickerCmp.jsx
+++ FrontEnd/src/Components/DatePickerCmp.jsx
@@ -2,13 +2,20 @@ import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import dayjs from "dayjs";
 
 //using default date picker by MUI
 const DatePickerCmp = () => {
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <DemoContainer components={["DatePicker"]}>
-        <DatePicker sx={{ left: "75%" }} label="Select Date" />
+        <DatePicker
+          sx={{ left: "75%" }}
+          label="Select Date"
+          closeOnSelect={true}
+          defaultValue={dayjs()}
+          inputFormat
+        />
       </DemoContainer>
     </LocalizationProvider>
   );
