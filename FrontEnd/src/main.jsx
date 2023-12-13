90ce51504859b692841d74a3294214e47ea50baa
zain <zainsattar21@gmail.com>
2023-12-13 13:07:51 +0500

    toast added on succefully login

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import UserProvider from "./Components/UserProvider.jsx";
+import { Toaster } from "react-hot-toast";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <UserProvider>
       <App />
+      <Toaster />
     </UserProvider>
   </React.StrictMode>
 );
